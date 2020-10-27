8fc644fd996447bfac8aa9b38c1650f31ce67906
Madhumitha M <madhumaruthu369@gmail.com>
2020-10-27 13:04:01 +0530

    This is a bot which does calculations using Python activities and displays the output to the user ...

---
+++ RPA Challenge Day 29 and 30/Add.py
@@ -0,0 +1,7 @@
+import os 
+
+def add ( ) :
+           num1 = 20
+           num2 = 10
+           result = num1 + num2
+           return result
---
+++ RPA Challenge Day 29 and 30/Substract.py
@@ -0,0 +1,7 @@
+import os
+
+def  substract ( ) :
+                  num1 = 20
+                  num2 = 10
+                  result = num1 - num2
+                  return result
